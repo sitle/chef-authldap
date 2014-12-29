5848c6e31b76a991b9743bd16193212ce0518429
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-12-28 22:02:47 -1000

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'chef-authldap'
+maintainer 'Leonard TAVAE'
+maintainer_email 'leonard.tavae@informatique.gov.pf'
+license 'Apache 2.0'
+description 'Installs/Configures chef-authldap'
+long_description 'Installs/Configures chef-authldap'
+version '0.1.0'
